416f4e6ecd518b8ea7647296e57b7b1c71aa3d08
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-09-16 21:30:56 -0300

    feat: implements `PUT`, `PATCH` and `DELETE` exemples

--- Program.cs
+++ Program.cs
@@ -19,5 +19,20 @@ app.MapPost("/", (string nome) =>
     return Results.Ok($"Retornando o nome enviado via parâmetro:{nome}");
 });
 
+app.MapPut("/usuarios/{id}", (int id, string novoNome) =>
+{
+    return Results.Ok($"Dado {id} atualizado com sucesso para {novoNome}");
+});
+
+app.MapPatch("/usuarios/{id}", (int id, string campo, string valor) =>
+{
+    return Results.Ok($"Dado {id} teve o campo {campo} atualizado para {valor}");
+});
+
+app.MapPatch("/usuarios/{id}", (int id) =>
+{
+    return Results.Ok($"Dado {id} foi removido com sucesso");
+});
+
 app.Run();
                                                                                                        
