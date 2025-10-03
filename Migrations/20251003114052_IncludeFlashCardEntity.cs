ed7321303d2ef074d25b8863eff824857555d3b7
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-10-03 08:42:24 -0300

    feat: add `FlashCard` model and migrations

---
+++ Migrations/20251003114052_IncludeFlashCardEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyStudies.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncludeFlashCardEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FlashCard",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Question = table.Column<string>(type: "TEXT", nullable: false),
+                    Answer = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StudyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlashCard", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FlashCard_Studies_StudyId",
+                        column: x => x.StudyId,
+                        principalTable: "Studies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlashCard_StudyId",
+                table: "FlashCard",
+                column: "StudyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlashCard");
+        }
+    }
+}
--- Model/FlashCard.cs
+++ Model/FlashCard.cs
@@ -5,15 +5,14 @@ using System.Threading.Tasks;
 
 namespace MyStudies.Model
 {
-    public class Study
+    public class FlashCard
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Content { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-        public List<Subject>? Subjects { get; set; }
-        public List<FlashCard>? FlashCards { get; set; }
+        public int StudyId { get; set; }
+
     }
 }
